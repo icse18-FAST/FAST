7670c51277ddffaeb150a50551ac6b78d7d50655
icse18-FAST <icse18.fast@gmail.com>
2017-08-31 17:20:08 +0200

    Updated .py files' header

--- tools/clean-preprocessed-input.py
+++ tools/clean-preprocessed-input.py
@@ -1,3 +1,21 @@
+'''
+This file is part of an ICSE'18 submission that is currently under review. 
+For more information visit: https://github.com/icse18-FAST/FAST.
+    
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this source.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import os
 
 
